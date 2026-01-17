f3aa4e316b7a6da03b4647a9ed3d20b4f3e68bb4
root <root@fp-hp.localdomain>
2026-01-17 17:27:04 +0100

    stabile - working on setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Installing Python dependencies"
-pip install -r requirements.txt
+pip install -r req.txt
 
 echo "Installing system deps for Playwright Chromium (Ubuntu 24.04+ compatible)"
 sudo apt update
