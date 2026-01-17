9a91f204f7c6ac6a20764afcbc6efe1cfbe10b34
root <root@fp-hp.localdomain>
2026-01-17 17:03:30 +0100

    stabile - working on setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Installing Python dependencies"
-pip install -r requirements.txt
+pip install -r req.txt
 
 echo "Installing Playwright Chromium browser"
 python -m playwright install chromium
